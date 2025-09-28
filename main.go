661da9f56911193114eb72f65e1b9892c48c23ba
Léo Peltier <contact@leo-peltier.fr>
2025-09-28 19:04:41 +0200

    Replace usage of QMap with TypedMap in nod export

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"goldutil/goldsrc"
 	"goldutil/goldsrc/typedmap"
 	"goldutil/neat"
-	"goldutil/qmap"
 	"goldutil/set"
 	"goldutil/sprite"
 	"goldutil/wad"
@@ -369,14 +368,6 @@ func doMapExport(cCtx *cli.Context) error {
 	return nil
 }
 
-func loadMap(path string) (qmap.QMap, error) {
-	if path == "" {
-		return qmap.LoadFromReader(os.Stdin)
-	} else {
-		return qmap.LoadFromFile(path)
-	}
-}
-
 func loadTypedMap(path string) (typedmap.TypedMap, error) {
 	if path == "" {
 		return typedmap.LoadFromReader(os.Stdin)
@@ -596,25 +587,25 @@ func doNodExport(cCtx *cli.Context) error {
 
 	var (
 		original = cCtx.Bool("original-positions")
-		entities = make([]qmap.Entity, 0, len(nodes)+len(links))
+		entities = make([]typedmap.AnonymousEntity, 0, len(nodes)+len(links))
 	)
 
 	for i, v := range nodes {
-		entities = append(entities, qmap.NewEntity(map[string]string{
-			qmap.KClass:  v.ClassName(),
-			qmap.KOrigin: v.Position(original).String(),
-			qmap.KName:   fmt.Sprintf("node#%d", i),
-		}))
+		entities = append(entities, typedmap.AnonymousEntity{KVs: map[string]string{
+			"classname":  v.ClassName(),
+			"origin":     v.Position(original).String(),
+			"targetname": fmt.Sprintf("node#%d", i),
+		}})
 	}
 
 	for linkTypeBitID := 0; linkTypeBitID <= goldsrc.LinkTypeBitMax; linkTypeBitID++ {
-		entities = append(entities, qmap.NewEntity(map[string]string{
-			qmap.KClass:            "func_group",
+		entities = append(entities, typedmap.AnonymousEntity{KVs: map[string]string{
+			"classname":            "func_group",
 			"_tb_type":             "_tb_layer",
 			"_tb_name":             fmt.Sprintf("hull#%d links (%s)", linkTypeBitID, goldsrc.LinkTypeName(linkTypeBitID)),
 			"_tb_id":               strconv.Itoa(linkTypeBitID + 1),
 			"_tb_layer_sort_index": strconv.Itoa(linkTypeBitID + 1),
-		}))
+		}})
 
 		for _, v := range links {
 			if (v.LinkInfo & (1 << linkTypeBitID)) == 0 {
@@ -622,16 +613,17 @@ func doNodExport(cCtx *cli.Context) error {
 			}
 
 			src := entities[v.SrcNode]
-			src.SetProperty("target", fmt.Sprintf("node#%d", v.DstNode))
-			src.SetProperty("_tb_layer", strconv.Itoa(linkTypeBitID+1))
+			src.KVs["target"] = fmt.Sprintf("node#%d", v.DstNode)
+			src.KVs["_tb_layer"] = strconv.Itoa(linkTypeBitID + 1)
 			entities = append(entities, src)
 		}
 	}
 
-	var out qmap.QMap
-	for _, v := range entities {
-		out.AddEntity(v)
+	out := typedmap.New()
+	if err := out.AddAnonymousEntities(entities...); err != nil {
+		return fmt.Errorf("unable to append entities to output map: %w", err)
 	}
+
 	fmt.Println(out.String())
 
 	return nil
